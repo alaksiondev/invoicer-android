3b5877d334625c25eba024517b1b362e0f52591c
lucca <lbeurmann.dev@gmail.com>
2024-11-10 23:13:06 -0300

    feat: setup plugins

---
+++ build-logic/src/main/kotlin/build/logic/configs/AppConfig.kt
@@ -0,0 +1,13 @@
+package build.logic.configs
+
+import org.gradle.api.JavaVersion
+
+internal object AppConfig {
+    val compileSdk = 34
+    val minSdk = 29
+    val versionCode = 1
+    val versionName = "1.0"
+
+    val javaVersion = JavaVersion.VERSION_11
+    val jvmTarget = "11"
+}
---
+++ build-logic/src/main/kotlin/build/logic/plugins/LibraryPlugin.kt
@@ -0,0 +1,10 @@
+package build.logic.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LibraryPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}

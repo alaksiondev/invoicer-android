c4eefda50c8ab5abc5e9ce2acf20c44fdb3809e0
Lucca Beurmann <luccabeurmann@Luccas-MacBook-Air.local>
2024-12-04 16:56:22 -0300

    feat: build sign in use case

---
+++ features/auth/domain/src/main/kotlin/features/auth/domain/usecase/SignInUseCase.kt
@@ -0,0 +1,28 @@
+package features.auth.domain.usecase
+
+import features.auth.domain.repository.AuthRepository
+import features.auth.domain.storage.AuthStorage
+
+interface SignInUseCase {
+    suspend fun signIn(
+        email: String,
+        password: String
+    )
+}
+
+internal class SignInUseCaseImpl(
+    private val authRepository: AuthRepository,
+    private val authStorage: AuthStorage
+) : SignInUseCase {
+
+    override suspend fun signIn(email: String, password: String) {
+        val authResponse = authRepository.signIn(
+            email = email,
+            password = password
+        )
+
+        authStorage.storeRefreshToken(token = authResponse.refreshToken)
+
+        authStorage.storeAccessToken(token = authResponse.accessToken)
+    }
+}

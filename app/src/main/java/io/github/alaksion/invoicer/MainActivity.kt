71b475a8465a06346bd65d74801d57d1e3780727
Lucca Beurmann <luccabeurmann@Luccas-MacBook-Air.local>
2024-12-05 16:33:03 -0300

    feat: add auth events subscriber

--- app/src/main/java/io/github/alaksion/invoicer/MainActivity.kt
+++ app/src/main/java/io/github/alaksion/invoicer/MainActivity.kt
@@ -1,17 +1,26 @@
 package io.github.alaksion.invoicer
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.view.WindowCompat
 import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import foundation.auth.impl.watcher.AuthEvent
+import foundation.auth.impl.watcher.AuthEventSubscriber
 import foundation.design.system.theme.InvoicerTheme
 import foundation.navigation.InvoicerScreen
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+
+    private val authSubscriber: AuthEventSubscriber by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +31,19 @@ class MainActivity : ComponentActivity() {
                     screens = listOf(ScreenRegistry.get(InvoicerScreen.Auth.AuthMenu)),
                 ) { navigator ->
                     SlideTransition(navigator)
+
+                    AuthEventEffect(
+                        subscriber = authSubscriber,
+                        onSignIn = {
+                            Toast.makeText(this, "Sign In Success", Toast.LENGTH_LONG)
+                                .show()
+                        },
+                        onSignOff = {
+                            navigator.replaceAll(
+                                ScreenRegistry.get(InvoicerScreen.Auth.AuthMenu)
+                            )
+                        }
+                    )
                 }
             }
         }
@@ -30,5 +52,21 @@ class MainActivity : ComponentActivity() {
     private fun forceLightStatusBar() {
         WindowCompat.getInsetsController(window, window.decorView)
             .isAppearanceLightStatusBars = false
+    }
+
+    @Composable
+    private fun AuthEventEffect(
+        subscriber: AuthEventSubscriber,
+        onSignIn: () -> Unit,
+        onSignOff: () -> Unit
+    ) {
+        LaunchedEffect(subscriber) {
+            subscriber.events.collect {
+                when (it) {
+                    AuthEvent.SignIn -> onSignIn()
+                    is AuthEvent.SignOff -> onSignOff()
+                }
+            }
+        }
     }
 }

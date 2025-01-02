da25a0efc284199287f81d58ecc9bb4e1db1b974
Lucca Beurmann <lbeurmann.dev@gmail.com>
2025-01-02 01:26:45 -0300

    feat: scaffold flow

---
+++ features/beneficiary/presentation/src/main/kotlin/features/beneficiary/presentation/screen/create/steps/BeneficiaryConfirmationStep.kt
@@ -0,0 +1,11 @@
+package features.beneficiary.presentation.screen.create.steps
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+internal class BeneficiaryConfirmationStep : Screen {
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ features/beneficiary/presentation/src/main/kotlin/features/beneficiary/presentation/screen/create/steps/BeneficiarySwiftStep.kt
@@ -0,0 +1,12 @@
+package features.beneficiary.presentation.screen.create.steps
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+internal class BeneficiarySwiftStep: Screen {
+
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}

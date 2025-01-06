e8e1dd925af3846b163b438be5a3720fce8faabc
Lucca Beurmann <lbeurmann.dev@gmail.com>
2025-01-05 23:01:45 -0300

    feat: fix iban screen

--- features/beneficiary/presentation/src/main/kotlin/features/beneficiary/presentation/screen/create/steps/BeneficiaryIbanStep.kt
+++ features/beneficiary/presentation/src/main/kotlin/features/beneficiary/presentation/screen/create/steps/BeneficiaryIbanStep.kt
@@ -3,7 +3,6 @@ package features.beneficiary.presentation.screen.create.steps
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +13,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.koin.koinScreenModel
+import cafe.adriel.voyager.koin.koinNavigatorScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import features.auth.design.system.components.spacer.Spacer
 import features.beneficiary.presentation.R
 import features.beneficiary.presentation.screen.create.CreateBeneficiaryScreenModel
@@ -24,20 +24,19 @@ import features.beneficiary.presentation.screen.create.components.BeneficiaryBas
 internal class BeneficiaryIbanStep : Screen {
     @Composable
     override fun Content() {
-        val screenModel = koinScreenModel<CreateBeneficiaryScreenModel>()
+        val navigator = LocalNavigator.currentOrThrow
+        val screenModel = navigator.koinNavigatorScreenModel<CreateBeneficiaryScreenModel>()
         val state by screenModel.state.collectAsStateWithLifecycle()
-        val navigator = LocalNavigator.current
 
         StateContent(
             iban = state.iban,
             onIbanChange = screenModel::updateIban,
             buttonEnabled = state.ibanIsValid,
-            onBack = { navigator?.pop() },
-            onContinue = { navigator?.push(BeneficiaryBankInfoStep()) }
+            onBack = { navigator.pop() },
+            onContinue = { navigator.push(BeneficiaryBankInfoStep()) }
         )
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun StateContent(
         iban: String,

e5bf82aa20ffcdbcb321a5752c904965af7d7f8a
Alaksion <lbeurmann.dev@gmail.com>
2025-01-28 20:18:45 -0300

    feat: fix navigation

--- features/invoice/presentation/src/main/kotlin/features/invoice/presentation/screens/feedback/InvoiceFeedbackScreen.kt
+++ features/invoice/presentation/src/main/kotlin/features/invoice/presentation/screens/feedback/InvoiceFeedbackScreen.kt
@@ -9,18 +9,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import features.auth.design.system.components.feedback.Feedback
 import features.invoice.presentation.R
 import foundation.design.system.tokens.Spacing
+import foundation.navigation.InvoicerScreen
 
 internal class InvoiceFeedbackScreen : Screen {
 
     @Composable
     override fun Content() {
-        val navigator = LocalNavigator.current?.parent
-        StateContent(onClearFlow = { navigator?.pop() })
+        val navigator = LocalNavigator.current
+
+        StateContent(
+            onClearFlow = {
+                navigator?.replaceAll(ScreenRegistry.get(InvoicerScreen.Home))
+            }
+        )
     }
 
     @Composable

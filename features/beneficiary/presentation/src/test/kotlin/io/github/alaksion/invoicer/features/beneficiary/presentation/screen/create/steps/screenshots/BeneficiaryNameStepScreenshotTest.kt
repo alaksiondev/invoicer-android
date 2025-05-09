dcc623eb1111262c949d3e404463cb597334ffba
Alaksion <lbeurmann.dev@gmail.com>
2025-05-09 17:04:07 -0300

    feat: create benefeciary screenshots

---
+++ features/beneficiary/presentation/src/test/kotlin/io/github/alaksion/invoicer/features/beneficiary/presentation/screen/create/steps/screenshots/BeneficiaryNameStepScreenshotTest.kt
@@ -0,0 +1,48 @@
+package io.github.alaksion.invoicer.features.beneficiary.presentation.screen.create.steps.screenshots
+
+import androidx.compose.runtime.Composable
+import app.cash.paparazzi.Paparazzi
+import foundation.designsystem.theme.InvoicerTheme
+import io.github.alaksion.invoicer.features.beneficiary.presentation.screen.create.steps.BeneficiaryNameStep
+import org.junit.Rule
+import org.junit.Test
+
+class BeneficiaryNameStepScreenshotTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi()
+
+    @Test
+    fun nameStep_default() {
+        paparazzi.snapshot {
+            TestContent(
+                name = ""
+            )
+        }
+    }
+
+    @Test
+    fun nameStep_filled() {
+        paparazzi.snapshot {
+            TestContent(
+                name = "Beneficiary name"
+            )
+        }
+    }
+
+    @Composable
+    private fun TestContent(
+        name: String
+    ) {
+        InvoicerTheme {
+            BeneficiaryNameStep()
+                .StateContent(
+                    name = name,
+                    onNameChange = { },
+                    onBack = { },
+                    onContinue = { },
+                    buttonEnabled = false,
+                )
+        }
+    }
+}

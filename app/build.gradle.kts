21ee53b5b9a50d54451406c12f4f351549156962
Alaksion <lbeurmann.dev@gmail.com>
2025-05-21 13:23:15 -0300

    feat: clean up app dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,62 +17,13 @@ android {
 }
 
 dependencies {
+    implementation(compose.foundation)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.splashscreen)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
-    // Firebase
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.crashlytics)
-    implementation(libs.firebase.analytics)
-
-    // Voyager
-    implementation(libs.voyager.navigator)
-    implementation(libs.voyager.transitions)
-    implementation(libs.voyager.koin)
-    implementation(libs.voyager.screenmodel)
-
-    // Koin
-    implementation(platform(libs.koin.bom))
-    implementation(libs.koin.core)
-    implementation(libs.koin.android)
-
-    // Foundation
-    implementation(projects.foundation.navigation)
-    implementation(projects.foundation.designSystem)
-    implementation(projects.foundation.network)
-    implementation(projects.foundation.validator)
-    implementation(projects.foundation.storage)
-    implementation(projects.foundation.auth)
-    implementation(projects.foundation.network)
-    implementation(projects.foundation.utils)
-
-    // Features
-    implementation(projects.features.auth.presentation)
-    implementation(projects.features.home)
-    implementation(projects.features.invoice)
-    implementation(projects.foundation.watchers)
-    implementation(projects.features.beneficiary.services)
-    implementation(projects.features.beneficiary.presentation)
-    implementation(projects.foundation.watchers)
-    implementation(projects.foundation.analytics)
-    implementation(projects.features.intermediary.services)
-    implementation(projects.features.intermediary.presentation)
-    implementation(projects.foundation.watchers)
-    implementation(projects.features.qrcodeSession)
+    // App
     implementation(projects.sharedApp)
 }

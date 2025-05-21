3561a9b6786a6733b8687e77e070c364437468e0
Alaksion <lbeurmann.dev@gmail.com>
2025-05-21 20:05:25 -0300

    feat: start koin modules on ios koin init

--- sharedApp/src/iosMain/kotlin/io/github/alaksion/invoicer/sharedApp/init/modules/platform/KoinModule.kt
+++ sharedApp/src/iosMain/kotlin/io/github/alaksion/invoicer/sharedApp/init/modules/platform/KoinModule.kt
@@ -1,9 +1,13 @@
 package io.github.alaksion.invoicer.sharedApp.init.modules.platform
 
+import io.github.alaksion.invoicer.sharedApp.init.di.appModule
 import io.github.alaksion.invoicer.sharedApp.init.modules.ModuleInitializer
+import org.koin.core.context.startKoin
 
 actual class KoinModule : ModuleInitializer {
     override fun onStart() {
-
+        startKoin {
+            modules(appModule)
+        }
     }
 }

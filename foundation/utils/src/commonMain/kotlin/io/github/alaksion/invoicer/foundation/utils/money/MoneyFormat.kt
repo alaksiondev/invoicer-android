13654a0a65f20fbb5c74672ceb768e221c3e8f6c
Alaksion <lbeurmann.dev@gmail.com>
2025-05-21 16:34:39 -0300

    feat: fix non KMP usages

--- foundation/utils/src/commonMain/kotlin/io/github/alaksion/invoicer/foundation/utils/money/MoneyFormat.kt
+++ foundation/utils/src/commonMain/kotlin/io/github/alaksion/invoicer/foundation/utils/money/MoneyFormat.kt
@@ -1,11 +1,8 @@
 package io.github.alaksion.invoicer.foundation.utils.money
 
-import java.util.Locale
-
 private const val MoneyMultiplier = 100.0
 
 fun Long.moneyFormat(
-    locale: Locale = Locale.getDefault()
 ): String {
-    return "$${String.format(locale, "%.2f", this / MoneyMultiplier)}"
+    return "$${(this / MoneyMultiplier) * MoneyMultiplier} "
 }
